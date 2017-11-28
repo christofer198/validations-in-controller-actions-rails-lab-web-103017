18caaa2ea712fada9ab34d0222f0196e400f9e55
Christofer Johnson <christofer.d.johnson@gmail.com>
2017-11-28 20:59:01 +0000

    automatically backed up by learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,8 +10,9 @@ class PostsController < ApplicationController
   def update
     @post = Post.find(params[:id])
     @post.assign_attributes(post_params)
+    
     if @post.valid?
-      @post.update
+      @post.save
       redirect_to author_path(@post)
     else
       render :new
