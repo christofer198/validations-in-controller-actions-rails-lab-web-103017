559126e56749090e8f0c4a40cc7f0b88b82a6119
Christofer Johnson <christofer.d.johnson@gmail.com>
2017-11-28 20:56:08 +0000

    automatically backed up by learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,7 +8,22 @@ class PostsController < ApplicationController
   end
 
   def update
+
+    if @post.valid?
+      # If--and only if--the post is valid, do what we usually do.
+      @post.save
+      # This returns a status_code of 302, which instructs the browser to
+      # perform a NEW REQUEST! (AKA: throw @post away and let the show action
+      # worry about re-reading it from the database)
+      redirect_to post_path(@post)
+    else
+      # If the post is invalid, hold on to @post, because it is now full of
+      # useful error messages, and re-render the :new page, which knows how
+      # to display them alongside the user's entries.
+      render :edit
+    end
     @post.update(post_params)
+
 
     redirect_to post_path(@post)
   end
